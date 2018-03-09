4f6672878b5b40fc36256d6c589b38314bb817ff
tianshengjie <tianshengjie@howzhi.com>
2018-03-09 11:07:09 +0800

    modified setting.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const Koa = require('koa');
-const app = new Koa();
+const Koa = require('koa'); 
+const Router = require('koa-router'); 
+const app = new Koa(); 
+const router = new Router(); 
+router.get('/', async (ctx) => { 
+  let html = ` <ul> <li><a href="/hello">helloworld</a></li> <li><a href="/about">about</a></li> </ul> ` ;
+  ctx.body = html 
+}).get('/hello', async (ctx) => { ctx.body = 'helloworld' })
+.get('/about', async (ctx) => { ctx.body = 'about' });
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
-
+app.use(router.routes(), router.allowedMethods());
 app.listen(3000);
+console.log('app started at port 3000...');
--- webpack/setting.js
+++ webpack/setting.js
@@ -2,7 +2,7 @@ const process = require('process');
 const rootPath = process.cwd();
 
 let setting = {
-  entry: rootPath + '/src/js/',
+  entry: rootPath + '/src/client/js/',
   output: rootPath + '/dist/',
   publicPath: '/dist/',
   author: 'tianshengjie',
