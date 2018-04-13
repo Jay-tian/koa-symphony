6d8ddedbd0f296f110f02674ebc55a36f14fc579
tianshengjie <tianshengjie@howzhi.com>
2018-04-13 09:13:28 +0800

    add koa-static

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 // const routers = require(rootPath+'config/router.js');
 // const rootPath = './src/server/';
 const Koa = require('koa'); 
+const path = require('path');
 const Router = require('koa-router'); 
-var fs = require('fs');
+const koaStatic = require('koa-static');
+const staticPath = './static';
+const fs = require('fs');
 
 const app = new Koa();
 const router = new Router();
 router.get('/', async (ctx) => { 
   let html = `<ul> <li><a href="/">/</a></li> <li><a href="/login">login</a></li> </ul> ` ;
   ctx.body = html
+});
+
+// app.keys = ['im a newer secret', 'i like turtle'];
+// app.keys = new KeyGrip(['im a newer secret', 'i like turtle'], 'sha256');
+// this.cookies.set('name', 'tobi', { signed: true });
+
+app.use(koaStatic(
+  path.join( __dirname,  staticPath)
+))
+
+app.on('error', function(err, ctx){
+  log.error('server error', err, ctx);
 });
 
 app.use(router.routes(), router.allowedMethods());
--- webpack/setting.js
+++ webpack/setting.js
@@ -3,8 +3,8 @@ const rootPath = process.cwd();
 
 let setting = {
   entry: rootPath + '/src/client/js/',
-  output: rootPath + '/dist/',
-  publicPath: '/dist/',
+  output: rootPath + '/static/',
+  publicPath: '/static/',
   author: 'tianshengjie',
 };
 
