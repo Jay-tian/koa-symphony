2322e3e809b2d65a273dd12ef0f0e8057bfe46d9
tianshengjie <tianshengjie@howzhi.com>
2018-04-20 00:25:18 +0800

    update

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,4 +1,4 @@
 require('../less/main.less');
 const cd = require('codeages-design/dist/codeages-design.js');
-import 'codeages-design/dist/codeages-design.css';
+import 'codeages-design/src/less/codeages-design.less';
 window.cd = cd;
