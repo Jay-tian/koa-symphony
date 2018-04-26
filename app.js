cdac0219233072bd2b1d258f67c6990bdc6b6982
tianshengjie <tianshengjie@howzhi.com>
2018-04-26 21:59:48 +0800

    update

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
 // 设置快捷路径
 process.env.NODE_PATH = __dirname + '/src/server/';
 require('module').Module._initPaths();
-
 const Router = require('koa-router'); 
 const app = require('./src/server/kernel.js');
 const router = new Router();
 
-
 router.get('/', async function (ctx, next) {
-  return ctx.render('index/index.twig');
-});
-
-router.get('/index', async function (ctx, next) {
-  return ctx.render('index/index.twig', {});
+  return ctx.render('furrytail/index.twig', {});
 });
 
 router.get('/furrytail', async function (ctx, next) {
   return ctx.render('furrytail/index.twig', {});
 });
-
 app.use(router.routes());
 app.listen(3000);
 
