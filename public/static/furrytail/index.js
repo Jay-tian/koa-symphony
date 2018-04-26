79e912b7e3cdaddb326271f84ba0814c8b092e42
tianshengjie <tianshengjie@howzhi.com>
2018-04-26 10:47:27 +0800

    update

--- public/static/furrytail/index.js
+++
@@ -1,2 +0,0 @@
-/*! tianshengjie */
-webpackJsonp([2],{333:function(n,c,t){"use strict";t(334)},334:function(n,c){}},[333]);
--- public/static/index/index.js
+++
@@ -1,2 +0,0 @@
-/*! tianshengjie */
-webpackJsonp([1],{335:function(n,c,t){"use strict";t(336)},336:function(n,c){}},[335]);
