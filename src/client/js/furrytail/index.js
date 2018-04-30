56c14911a3f8846cc83ae1e44a0043bfd9009a18
tianshengjie <tianshengjie@howzhi.com>
2018-04-30 12:51:48 +0800

    update

--- src/client/js/furrytail/index.js
+++ src/client/js/furrytail/index.js
@@ -1,6 +1,12 @@
 require('../../less/pages/furrytail.less');
 require('jay-fullpage.js');
+let $fullpage = $('#fullpage');
 
-$('#fullpage').fullpage({
-  scrollingSpeed: 1000
+$fullpage.fullpage({
+  scrollingSpeed: 1000,
+  anchors:['furlosophy', 'byebyefur', 'fooddict',' scratcher', 'pills', 'addiction', 'aboot', 'contact']
+});
+
+$('.first-netxt').click(function(){
+  $fullpage.fullpage.moveSectionDown();
 });
