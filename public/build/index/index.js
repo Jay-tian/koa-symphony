0016190e3b679f24b89da42462aa2883f4e3568f
tianshengjie <tianshengjie@howzhi.com>
2018-04-30 12:52:43 +0800

    add static

--- public/build/index/index.js
+++ public/build/index/index.js
@@ -1,2 +1,2 @@
 /*! tianshengjie */
-webpackJsonp([1],{335:function(n,c,t){"use strict";t(336)},336:function(n,c){}},[335]);
+webpackJsonp([2],{336:function(n,c,t){"use strict";t(337)},337:function(n,c){}},[336]);
