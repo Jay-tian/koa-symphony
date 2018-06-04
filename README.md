13c5e6982337bb6c77bfc993e004fcdd6fadb3c5
tianshengjie <tianshengjie@howzhi.com>
2018-06-04 17:56:34 +0800

    change version

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # koa-easy
 
+## start
+基于 koa2的简单通用的系统。
+集成了twig引擎，已经webpack4
+
+## todo
+1. dao层封装
+2. service层封装
+3. 数据库主从备份
+4. 缓存服务器
+5. cluster负载均衡
 
--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 const app = require('./src/server/kernel.js');
-app.listen(3000);
-
-//module.exports = require('./src/server/kernel.js');
+module.exports = app;
