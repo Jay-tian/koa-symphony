175a66630039c966db669ea22858c4dca6ad8c9b
tianshengjie <tianshengjie@howzhi.com>
2018-08-13 11:21:08 +0800

    uploader

--- src/server/controller/SystemController.js
+++ src/server/controller/SystemController.js
@@ -10,25 +10,7 @@ class SystemController extends BaseController {
 
   imageUpload() {
     return async (ctx, next) => {
-      let body = ctx.request.body;
-      console.log(ctx.request.query);
-
-      const tmpdir = os.tmpdir();
-      const filePaths = [];
-     
-      console.log(ctx.request.body);
-      console.log(123123);
-      const files = ctx.request.body.files || {};
-      for (let key in files) {
-        const file = files[key];
-        const filePath = path.join(tmpdir, file.name);
-        const reader = fs.createReadStream(file.path);
-        const writer = fs.createWriteStream(filePath);
-        reader.pipe(writer);
-        filePaths.push(filePath);
-      }
-    
-      ctx.body = {path: 'lalalalal'};
+      ctx.body = {s: 'asd'}
       next();
     };      
   }
--- src/server/middleware/BodyParserMiddleware.js
+++ src/server/middleware/BodyParserMiddleware.js
@@ -1,6 +1,15 @@
 const koaBody = require('koa-body');
+const path = require('path');
+const parameters = require('../loader/ConfigLoader.js');
 
 module.exports = {
   priority: 0,
-  middleware: koaBody({ multipart: true }),
+  middleware: koaBody({
+    multipart:true, // 支持文件上传
+    encoding:'gzip',
+    formidable:{
+      uploadDir: path.join(parameters.rootPath,'public/data/'), // 设置文件上传目录
+      keepExtensions: true,    // 保持文件的后缀
+      maxFieldsSize:2 * 1024 * 1024, // 文件上传大小
+    }}),
 };
