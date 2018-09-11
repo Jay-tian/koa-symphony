29aaf88d5334220f8cb4ebda9b765baa42e2dd55
tianshengjie <tianshengjie@howzhi.com>
2018-09-11 13:15:44 +0800

    update

--- src/middleware/ListenerMiddleware.js
+++ src/middleware/ListenerMiddleware.js
@@ -1,22 +1,22 @@
 
 let middleware =  async (ctx, next) => {
   if (0 == ctx.request.url.indexOf('/admin') && !ctx.state.user.isAdmin()) {
-    ctx.redirect('back', '/');
+    ctx.redirect('/');
     return;
   }
 
   if (0 == ctx.request.url.indexOf('/my/') && !ctx.state.user.isLogin()) {
-    ctx.redirect('back', '/');
+    ctx.redirect('/');
     return;
   }
 
   if ((0 == ctx.request.url.indexOf('/login') || 0 == ctx.request.url.indexOf('/reigster')) && ctx.state.user.isLogin()) {
-    ctx.redirect('back', '/');
+    ctx.redirect('/');
     return;
   }
 
   if (0 == ctx.request.url.indexOf('/logout') && !ctx.state.user.isLogin()) {
-    ctx.redirect('back', '/');
+    ctx.redirect('/');
     return;
   }
   
