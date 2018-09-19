53de546af755034c80869c36e1e8a7af880d07a1
tianshengjie <tianshengjie@howzhi.com>
2018-09-19 09:38:17 +0800

    update

--- src/application/cropper.js
+++ src/application/cropper.js
@@ -1,13 +1,9 @@
 global.symphony.cropper = function(filePath, params) {
-<<<<<<< HEAD
-  let params = {
-=======
   params = {
->>>>>>> 60590fce53be452c1be697b8fc05b325063ce211
     width: 100,
     height: 100,
     originWidth: 200,
     x: 9,
     height: 10,
   }
-}
+};
