98c2da776e64e87230e9237893f1286d3cf8fc53
tianshengjie <tianshengjie@howzhi.com>
2018-09-25 19:09:09 +0800

    add 新增图片裁切

--- src/application/cropper.js
+++ src/application/cropper.js
@@ -1,9 +1,25 @@
+const easyimage =  require('easyimage');
+const stringRandom = require('string-random');
+const path = require('path');
+const fs = require('fs');
+
 global.symphony.cropper = function(filePath, params) {
-  params = {
-    width: 100,
-    height: 100,
-    originWidth: 200,
-    x: 9,
-    height: 10,
+  try {
+    let dst = stringRandom(16);
+    dst = path.dirname(filePath)+'/'+dst+'.'+path.extname(filePath)
+
+    easyimage.crop({
+      src: filePath,
+      dst: dst,
+      x: params.x,
+      y: params.y,
+      cropheight: params.height,
+      cropwidth: params.width,
+    })
+  } catch (e) {
+    console.log("Error: ", e);
   }
+
+  fs.unlink(filePath);
+  return dst;
 };
