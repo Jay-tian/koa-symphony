f3051a7b750f49e9f3af49989ca6c43c6d4618fd
tianshengjie <tianshengjie@howzhi.com>
2018-09-26 10:39:39 +0800

    add  cropper.js

--- src/application/cropper.js
+++ src/application/cropper.js
@@ -1,26 +1,26 @@
 const easyimage =  require('easyimage');
 const stringRandom = require('string-random');
 const path = require('path');
-const fs = require('fs');
 const toolkit = require('../toolkit/index');
+const fs = require('fs');
 
 global.symphony.cropper = function(filePath, params) {
   try {
-    let dst = stringRandom(16)+toolkit.timestamp();
-    dst = path.dirname(filePath)+'/'+dst+'.'+path.extname(filePath)
+    let dst = stringRandom(32)+toolkit.timestamp();
+    dst = path.dirname(filePath)+'/'+dst+path.extname(filePath)
 
-    easyimage.crop({
+    return easyimage.crop({
       src: filePath,
       dst: dst,
       x: params.x,
       y: params.y,
       cropheight: params.height,
       cropwidth: params.width,
+    }).then(function(data) {
+      fs.unlink(filePath);
+      return data;
     })
   } catch (e) {
     console.log("Error: ", e);
   }
-
-  fs.unlink(filePath);
-  return dst;
 };
