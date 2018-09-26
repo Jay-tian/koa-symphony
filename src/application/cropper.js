2ef7e3c3812156e17cc656b815bf04b8eaa1bfe9
tianshengjie <tianshengjie@howzhi.com>
2018-09-26 17:01:18 +0800

    update

--- src/application/cropper.js
+++ src/application/cropper.js
@@ -8,7 +8,6 @@ global.symphony.cropper = function(filePath, params) {
   try {
     let dst = stringRandom(32)+toolkit.timestamp();
     dst = path.dirname(filePath)+'/'+dst+path.extname(filePath)
-
     return easyimage.crop({
       src: filePath,
       dst: dst,
