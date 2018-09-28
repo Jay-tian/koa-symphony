bab42c417bab84b9657031d111eb3a6c725078f3
tianshengjie <tianshengjie@howzhi.com>
2018-09-28 13:45:06 +0800

    update

--- src/application/cropper.js
+++ src/application/cropper.js
@@ -1,26 +1,21 @@
-const easyimage =  require('easyimage');
+const gm = require('gm');
 const stringRandom = require('string-random');
 const path = require('path');
 const fs = require('fs');
 const toolkit = require('../toolkit/index');
 
 global.symphony.cropper = function(filePath, params) {
-  try {
-    let dst = stringRandom(16)+toolkit.timestamp();
-    dst = path.dirname(filePath)+'/'+dst+'.'+path.extname(filePath)
-
-    easyimage.crop({
-      src: filePath,
-      dst: dst,
-      x: params.x,
-      y: params.y,
-      cropheight: params.height,
-      cropwidth: params.width,
+  let dst = stringRandom(16)+toolkit.timestamp();
+  dst = path.dirname(filePath)+'/'+dst+path.extname(filePath)
+  return new Promise(function (resolve, reject) {
+    gm(filePath).crop(params.width, params.height, params.x, params.y)
+    .write(dst, function (err) {
+      fs.unlink(filePath);
+      resolve(dst);
+      if (!err) {
+        console.log('crazytown has arrived');
+        reject(dst);
+      }
     })
-  } catch (e) {
-    console.log("Error: ", e);
-  }
-
-  fs.unlink(filePath);
-  return dst;
+  });
 };
